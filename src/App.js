c22d9c90a258cb8db35018b64397162a67371438
Nishant Ketu <69218488+ketunishant@users.noreply.github.com>
2021-09-06 00:59:55 +0530

    videos no 5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar'
 function App() {
   return (
+    <>
+
+    <Navbar/>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+ </> );
 }
 
 export default App;
